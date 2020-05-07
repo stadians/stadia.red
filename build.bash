82bf877a9452f9235c08cd19fc8627d0bb5e147e
stadian <stadian@stadians.dev>
2020-05-07 17:10:24 +0000

    r56 alfa zero delta alfa

--- build.bash
+++ build.bash
@@ -2,10 +2,11 @@
 # expects to run through `yarn build`.
 set -vexuo pipefail;
 
-replace-in-files --regex='"version"\s*:\s*"(.*?)"' --replacement='"version": "'"$(date "+%Y.%-m.%-d")"'"' ./package.json ./src/manifest.json;
+replace-in-files --regex='"version"\s*:\s*"(.*?)"' --replacement='"version": "'"$(date --utc "+%Y.%-m.%-d")"'"' ./package.json ./src/manifest.json;
+replace-in-files --regex='"node"\s*:\s*"(.*?)"' --replacement='"node": "'"$(node --version)"'"' ./package.json;
 
 rm -rf ./docs/*;
-cp -r ./src/* ./data/* ./static/* ./package.json ./docs/;
+cp -r ./src/* ./data/* ./static/* ./docs/;
 cp ./src/index.html ./docs/.htm;
 sort-json ./*.json ./*/*.json;
 prettier --write src *.json */*.json;
