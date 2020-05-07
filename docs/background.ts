59ad77f611b4658a3504c7dcffbb1b024e8c853a
stadian <stadian@stadians.dev>
2020-05-07 03:26:56 +0000

    r53 charlie eight seven four

--- docs/background.ts
+++ docs/background.ts
@@ -1,13 +1,7 @@
 declare const chrome: any;
-const {
-  runtime: { getURL },
-  browserAction,
-  tabs,
-  windows,
-} = chrome;
 
-browserAction.onClicked.addListener(async (activeTab: any) => {
-  const url = getURL("./.htm");
+chrome.browserAction.onClicked.addListener(async (activeTab: any) => {
+  const url = chrome.runtime.getURL("./.htm");
 
   console.log({ activeTab });
 
@@ -16,26 +10,26 @@ browserAction.onClicked.addListener(async (activeTab: any) => {
   }
 
   const matchingTabs: Array<any> = await new Promise((resolve) =>
-    tabs.query(
+    chrome.tabs.query(
       {
-        url: getURL("*"),
+        url: chrome.runtime.getURL("*"),
       },
       resolve
     )
   );
 
   if (matchingTabs.length > 0) {
-    tabs.update(matchingTabs[0].id, { active: true, highlighted: true });
+    chrome.tabs.update(matchingTabs[0].id, { active: true, highlighted: true });
     for (const [i, tab] of matchingTabs.sort((a, b) => a.id - b.id).entries()) {
       if (i === 0) {
-        tabs.update(tab.id, { highlighted: true, active: true });
-        windows.update(tab.windowId, { focused: true });
+        chrome.tabs.update(tab.id, { highlighted: true, active: true });
+        chrome.windows.update(tab.windowId, { focused: true });
       } else {
-        tabs.update(tab.id, { highlighted: true });
+        chrome.tabs.update(tab.id, { highlighted: true });
       }
     }
   } else {
-    await new Promise((resolve) => tabs.create({ url }, resolve));
+    await new Promise((resolve) => chrome.tabs.create({ url }, resolve));
   }
 });
 
