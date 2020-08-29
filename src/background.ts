5f26b56282ec319e8a0ecf74b60e90beb4a1a0b4
stadian <stadian@stadians.dev>
2020-08-29 22:34:40 +0000

    r209 delta four nine nine

--- src/background.ts
+++ src/background.ts
@@ -1,29 +1,13 @@
-browser.browserAction.onClicked.addListener(async (activeTab: any) => {
-  const url = browser.runtime.getURL("./.htm");
-
-  console.log({ activeTab });
-
-  if (activeTab.url === url) {
-    return;
-  }
-
-  const matchingTabs: Array<any> = await browser.tabs.query({
-    url: browser.runtime.getURL("*"),
-  });
-
-  if (matchingTabs.length > 0) {
-    for (const [i, tab] of matchingTabs.sort((a, b) => a.id - b.id).entries()) {
-      if (i === 0) {
-        await browser.tabs.update(tab.id, {
-          highlighted: true,
-          active: true,
-        });
-        await browser.windows.update(tab.windowId, { focused: true });
-      } else {
-        browser.tabs.update(tab.id, { highlighted: true });
-      }
-    }
+browser.runtime.onMessageExternal.addListener(async (message, sender) => {
+  if (message?.fetch) {
+    const [path, ...rest] = message?.fetch;
+    const url = `https://stadia.google.com/${path.replace(/^\//, "")}`;
+    const response = await fetch(url, ...rest);
+    const text = await response.text();
+    return { text, status: response.status };
+  } else if (message?.ping) {
+    return "pong";
   } else {
-    await browser.tabs.create({ url });
+    return null;
   }
 });
