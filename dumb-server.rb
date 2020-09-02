6968992a5c662bd5ec7de0c763508d912ca093c8
stadian <stadian@stadians.dev>
2020-09-02 11:24:48 +0000

    r233 echo foxtrot charlie five

--- dumb-server.rb
+++ dumb-server.rb
@@ -2,33 +2,55 @@
 
 require 'socket'
 
+host = (ENV['HOST'] or "127.0.0.1").to_s
 port = (ENV['PORT'] or 57482).to_i
-server = TCPServer.new("127.0.0.1", port)
-puts "Listening at http://127.0.0.1:#{port}"
+server = TCPServer.new(host, port)
+puts "Listening at http://#{host}:#{port}"
 
 while connection = server.accept
   request = connection.gets
 
   header_chunk, body = request.partition("\r\n\r\n")
-  route, header_lines = header_chunk.partition("\r\n")
+  route_line, header_lines = header_chunk.partition("\r\n")
   headers = header_lines.split("\r\n")
+  route = route_line.sub(/[ ]+HTTP\/1\.[01]$/, '')
 
-  case route
-  when /^OPTIONS .*$/
-    connection.print "HTTP/1.1 204 No Content\r\n"
-  when /^PUT \/skus\.json HTTP\/1.1$/
-    File.write("./stadia.st/-/skus.json", body)
-    connection.print "HTTP/1.1 204 No Content\r\n"
-  when /^PUT \/index\.html HTTP\/1.1$/
-    File.write("./stadia.run/index.html", body)
-    connection.print "HTTP/1.1 204 No Content\r\n"
-  else
-    connection.print "HTTP/1.1 404 Not Found\r\n"
+  client = connection.addr[2]
+
+  response = nil
+  status = case route
+    when /^GET \/skus\.json$/
+      response = File.read("./stadia.st/-/skus.json", :encoding => "utf-8")
+      "200 OK"
+    when /^PUT \/skus\.json$/
+      File.write("./stadia.st/-/skus.json", body)
+      "204 No Content"
+
+    when /^GET \/index\.html$/
+      response = File.read("./stadia.run/index.html", :encoding => "utf-8")
+      "200 OK"
+    when /^PUT \/index\.html$/
+      File.write("./stadia.run/index.html", body)
+      "204 No Content"
+
+    when /^OPTIONS \/.+$/
+      "204 No Content"
+    else
+      "404 Not Found"
   end
 
+  puts "#{client}  |  #{route}  |  #{status}"
+
+  connection.print "HTTP/1.1 #{status}\r\n"
   connection.print "Access-Control-Allow-Origin: *\r\n"
   connection.print "Access-Control-Allow-Methods: *\r\n"
-  connection.print "\r\n"
+
+  unless response.nil?
+    connection.print "Content-Type: text/plain; charset=utf-8\r\n"
+    connection.print "Content-Length: #{response.bytesize}\r\n"
+    connection.print "\r\n"
+    connection.print response
+  end
 
   connection.close
 end
