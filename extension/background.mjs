cc2caf0b72757517cad9e14de842e25081879770
stadian <stadian@stadians.dev>
2020-09-02 20:39:28 +0000

    r246 seven alfa bravo nine

--- extension/background.mjs
+++ extension/background.mjs
@@ -5,18 +5,34 @@ chrome.runtime.onMessageExternal.addListener(
   }
 );
 
-const onMessage = async (message) => {
-  if (message?.fetch) {
-    const [path, ...rest] = message?.fetch;
-    const url = `https://stadia.google.com/${path.replace(/^\//, "")}`;
+const rpcs = {
+  async ping() {
+    return "pong";
+  },
+
+  async fetchStadia(path, ...rest) {
+    const url = `https://stadia.google.com/${path}`;
     const response = await fetch(url, ...rest);
     const text = await response.text();
-    return { text, status: response.status };
-  } else if (message?.ping) {
-    return "pong";
-  } else {
     return {
-      error: "what do you want?",
+      ok: response.ok,
+      statusText: response.statusText,
+      status: response.status,
+      redirected: response.redirected,
+      url: response.url,
+      _text: text,
     };
+  },
+};
+
+const onMessage = async (message) => {
+  const result = {};
+  for (const key of Object.keys(message)) {
+    const args = message[key];
+    try {
+      result[key] = { result: await rpcs[key](...args) };
+    } catch (error) {
+      result[key] = { error: error.toString() };
+    }
   }
 };
---
+++ usage.md
@@ -0,0 +1,5 @@
+# Usage
+
+## [stadia.run](https://stadia.run)
+
+The first thing to do is yell at me to finish this page.
